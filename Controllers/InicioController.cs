aa4a0ae8e1c9a56501607b8a2772f8ab6caaff79
Elvafranco <efrancoa3@miumg.edu.gt>
2023-11-05 01:12:46 -0600

    Program

--- Controllers/InicioController.cs
+++ Controllers/InicioController.cs
@@ -11,10 +11,7 @@ namespace appbeneficiencia.Controllers
     public class InicioController : Controller
     {
         private readonly IUsuarioService _usuarioServicio;
-        public InicioController(IUsuarioService usuarioServicio)
-        {
-            _usuarioServicio = usuarioServicio;
-        }
+        public InicioController(IUsuarioService usuarioServicio) => _usuarioServicio = usuarioServicio;
 
         public IActionResult Registrarse()
         {
